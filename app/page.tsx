6297cbc633f8564b621631c041a8591195caa5e1
@Prakul111 <iamprakul111@gmail.com>
2024-10-15 22:50:46 +0530

    ui-compo...

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+import { Button } from "@/components/ui/button"
 
 export default async function Home() {
-    <main>
+  return (
+    <main className="text-4xl">
       <h1>Homepage</h1>
+      <Button> Click Me</Button>
     </main>
+  )
 }
